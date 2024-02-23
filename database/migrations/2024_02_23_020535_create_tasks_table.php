d3f07bf8fd5d5c45a1f936eb929fb81149e32f46
saeed <saeedmouzarmi@gmail.com>
2024-02-23 07:54:54 +0330

    Added migrations.

---
+++ database/migrations/2024_02_23_020535_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enums\TaskStatusEnum;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained('users');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('status')->default(TaskStatusEnum::getDefaultStatus());
+            $table->timestamp("deadline")->nullable();
+            $table->timestamp("completed_at")->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
