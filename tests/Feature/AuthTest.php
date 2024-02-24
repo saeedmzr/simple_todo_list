f7e34d6caa2ea7f5740eb671a7705cb52bfe54ed
saeed <saeedmouzarmi@gmail.com>
2024-02-24 16:30:51 +0330

    Created tests for login, register and logout endpoints.

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -20,4 +20,71 @@ class AuthTest extends TestCase
 
         $this->taskRepository = new TaskRepository(new Task());
     }
+
+    /** @test */
+    public function user_can_login()
+    {
+        $desiredPassword = "12345678";
+        $createdUser = User::factory()->create([
+            'password' => $desiredPassword
+        ]);
+
+        $data = [
+            'email' => $createdUser->email,
+            'password' => $desiredPassword,
+        ];
+
+        $response = $this->postJson('/api/auth/login', $data);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'access_token',
+                ],
+            ]);
+    }
+
+    /** @test */
+    public function user_can_register()
+    {
+        $desiredPassword = "12345678";
+        $desiredEmail = "test@test.com";
+
+
+        $data = [
+            'name' => "Test name",
+            'email' => $desiredEmail,
+            'password' => $desiredPassword,
+        ];
+
+        $response = $this->postJson('/api/auth/register', $data);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    'access_token',
+                ],
+            ]);
+        $this->assertDatabaseHas('users', [
+            "email" => $desiredEmail
+        ]);
+    }
+
+    /** @test */
+    public function user_can_logout()
+    {
+        $desiredPassword = "12345678";
+
+        $createdUser = User::factory()->create([
+            'password' => $desiredPassword
+        ]);
+
+        $this->actingAs($createdUser);
+
+
+        $response = $this->postJson('/api/auth/logout');
+        $response->assertStatus(200);
+
+    }
+
 }
